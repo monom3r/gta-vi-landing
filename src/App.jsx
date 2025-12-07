cfebc7609988265e5971a4e62387055361344757
monom3r <ysnbakr5@gmail.com>
2025-12-07 03:56:09 +0300

    clean up the boilerplate, add the assets, and set up the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
